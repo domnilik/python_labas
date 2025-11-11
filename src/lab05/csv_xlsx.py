8c9f06f442a28a647a521be834738e95d50b543f
89234447300 <donskaya.lika07@mail.ru>
2025-11-11 12:33:05 +0300

    add 04

--- src/lab05/csv_xlsx.py
+++ src/lab05/csv_xlsx.py
@@ -10,23 +10,23 @@ def csv_to_xlsx(csv_path: str, xlsx_path: str) -> None:
     with open(csv_path, encoding="utf-8") as f:
         data = list(csv.reader(f))
 
-    if not data:
+    if not data: #если данных нет
         raise ValueError("CSV файл пустой")
 
     from openpyxl import Workbook
-    wb = Workbook()
-    ws = wb.active
+    wb = Workbook() #создаем рабочую книгу экселы
+    ws = wb.active #берем первую страницу
     ws.title = "Sheet1"
 
     for row in data:
-        ws.append(row)
+        ws.append(row) #добавляем эту строку в Excel лист
 
-    for column in ws.columns:
+    for column in ws.columns: #проходим по всем колонкам
         max_length = 0
-        for cell in column:
-            if cell.value:
-                max_length = max(max_length, len(str(cell.value)))
-        ws.column_dimensions[column[0].column_letter].width = max(max_length + 2, 8)
+        for cell in column: #проходим по всем ячейкам
+            if cell.value: #если ячейка не пустая
+                max_length = max(max_length, len(str(cell.value))) #считаем длину текста в ячейке и определяем максимальную длину
+        ws.column_dimensions[column[0].column_letter].width = max(max_length + 2, 8) #получаем буквы колонки, ширина + 2 пробела, не менее 8 символов
 
     wb.save(xlsx_path)
 
@@ -38,6 +38,6 @@ if __name__ == "__main__":
             str(base_dir / "data" / "samples" / "people.csv"),
             str(base_dir / "data" / "out" / "people.xlsx")
         )
-        print("CSV → XLSX")
+        print("CSV to XLSX")
     except Exception as e:
         print(f"{e}")
